80fe65ae55a5b4912443de97762a5b7553fcb4c8
yaolinjia <loadingyao@163.com>
2024-06-11 00:01:57 +0800

    no message

--- api/app.js
+++ api/app.js
@@ -5,3 +5,7 @@ import request from '@/utils/request.js';
 export const appIndex = function(data = {}) {
 	return request.post('/index/index', data)
 }
+//消息列表
+export const msgList = function(data = {}) {
+	return request.get('/message/data_log', data)
+}
